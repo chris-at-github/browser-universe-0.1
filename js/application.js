e18e12a20e1118915d97138ca9bc384ff4666f35
Christian Pschorr <pschorr.christian@gmail.com>
2014-03-17 07:47:16 +0100

    first runable version

--- js/application.js
+++ js/application.js
@@ -1,33 +1,56 @@
-Application = Ember.Application.create();
-Application.ApplicationAdapter = DS.FixtureAdapter.extend();
+App = Ember.Application.create();
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
-Application.Router.map(function() {
-  this.resource('planet-layer', {
-  	path: '/'
-  });
+App.ObjectTransform = DS.Transform.extend({
+  deserialize: function(serialized) {
+    return serialized;
+  },
+  serialize: function(deserialized) {
+    return deserialized;
+  }
 });
 
-Application.PlanetsRoute = Ember.Route.extend({
+App.Planet = DS.Model.extend({
+  color: DS.attr('string'),
+  position: DS.attr('object'),
+	style: function() {
+	    return 'top:' + this.get('position').y + 'px; left:' + this.get('position').x + 'px;';
+	  }.property('style'),  
+});
+
+App.Planet.FIXTURES = [
+	{
+		id: 1,
+		color: 'blue',
+		position: {
+			x: 15,
+			y: 15
+		}
+	},
+	{
+		id: 2,
+		color: 'blue',
+		position: {
+			x: 30,
+			y: 30
+		}
+	},
+	{
+		id: 3,
+		color: 'blue',
+		position: {
+			x: 45,
+			y: 45
+		}
+	},
+];
+
+App.PlanetsRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('planet');
 	}
 });
 
-Application.Planet = DS.Model.extend({
-  color: DS.attr('string')
+App.Router.map(function() {
+  this.resource('planets', { path: '/' });
 });
-
-Application.Planet.FIXTURES = [
-	{
-		id: 1,
-		color: 'blue'
-	},
-	{
-		id: 2,
-		color: 'blue'
-	},
-	{
-		id: 3,
-		color: 'blue'
-	},
-];
